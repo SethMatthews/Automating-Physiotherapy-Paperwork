01748544eb562489b4a12512d7df0751b279533b
Seth Matthews <sethmatthews@Seths-MacBook-Pro-2.local>
2023-07-17 12:00:07 +1000

    update object to useState and checkboxes is now also working

--- src/components/GenerateTool.tsx
+++ src/components/GenerateTool.tsx
@@ -8,9 +8,12 @@ import FormResults from './FormResults';
 
 export default function GenerateTool() {
 
-  const [results,setResults] = useState<string[]>([]);
+  // const [results,setResults] = useState<string[]>([]);
 
-  let resultsToDisplay: { [questionName: string]: string[]} = {};
+  // let resultsToDisplay: { [questionName: string]: string[]} = {};
+
+  const [resultsToDisplay,setResultsToDisplay] = useState<{[questionName: string]: string[]}>({});
+
 
 
 
@@ -40,9 +43,7 @@ export default function GenerateTool() {
   //add new question with intial value 
 
   const addNewQuestion = (questionName: string, initialValue: string, resultsToDisplay:{[questionName: string]: string[]}) =>{
-    // if(typeof resultsToDisplay === "undefined"){
-    //   throw new Error('resultsToDisplay is undefined');
-    // }
+
     const newResultsToDisplay = resultsToDisplay;
     newResultsToDisplay[questionName]=[initialValue];
     return newResultsToDisplay;
@@ -85,26 +86,17 @@ export default function GenerateTool() {
 
     if (target.type === 'radio') {
         console.log("radio fired");
-        resultsToDisplay = addNewRadioValue(target.name.toString(), target.value.toString(), resultsToDisplay);
+        setResultsToDisplay(addNewRadioValue(target.name.toString(), target.value.toString(), resultsToDisplay));
 
     }
     if (target.type === 'checkbox') {
         console.log("checkbox fired");
         if (target.checked){
             console.log("Checked")
-            const updatedArray: string[] = results;
-            updatedArray.push(target.value.toString());            
-            const sliceArray: string[] = updatedArray.slice();
-            setResults(sliceArray);
-            resultsToDisplay = addNewCheckboxValue(target.name.toString(), target.value.toString(), resultsToDisplay); 
+            setResultsToDisplay(addNewCheckboxValue(target.name.toString(), target.value.toString(), resultsToDisplay)); 
         }
         if (!target.checked){
-            const filteredArray = results.filter(item=>{
-                return item !== target.value.toString();
-            });
-            setResults(filteredArray);
-            console.log(results);
-            resultsToDisplay = removeCheckboxValue(target.name.toString(), target.value.toString(), resultsToDisplay);
+          setResultsToDisplay(removeCheckboxValue(target.name.toString(), target.value.toString(), resultsToDisplay));
         }
     }
     console.log("resultsToDisplay is ",resultsToDisplay);
