36893b6eed7d818402b21442bfbc481a5f450493
Seth Matthews <98sethmatthews@gmail.com>
2023-08-08 11:57:39 +1000

    added chronic drop down

---
+++ src/components/questions/WhatIsTheNatureOfPain/ChronicDropdown.tsx
@@ -0,0 +1,27 @@
+
+const ChronicDropdown= ({selectedOption, parentQuestionPath}:{selectedOption:string , parentQuestionPath: string}) => {
+
+    // const currentQuestionPath = questionName+"-"+selectedOption;
+
+    return ( 
+            <div>
+                <div  id="input" className="m-1 border-gray-100 border-2 rounded-md py-3 px-1 hover:shadow-lg">
+                    <select className="ml-3 rounded-sm" name={parentQuestionPath} id={parentQuestionPath}>
+                        <option value="1">1 </option>
+                        <option value="2">2 </option>
+                        <option value="3">3 </option>
+                        <option value="4">4 </option>
+                        <option value="5">5 </option>
+                        <option value="5+">5+ </option>
+                        <option value="10+">10+ </option>
+                        <option value="20+">20+ </option>
+                    </select>
+                    <label className="ml-3" htmlFor={parentQuestionPath}>years</label>
+                </div>
+
+            </div> 
+            
+    );
+}
+ 
+export default ChronicDropdown;
--- src/components/questions/WhatIsTheNatureOfPain/WhatIsTheNatureOfPainNewOptionChronic.tsx
+++ src/components/questions/WhatIsTheNatureOfPain/WhatIsTheNatureOfPainNewOptionChronic.tsx
@@ -1,6 +1,8 @@
 
 import RadioInputOption from "~/components/RadioInput";
 import HowLongFor from "./HowLongFor";
+import HowLongForOption from "./HowLongForOption";
+import ChronicDropdown from "./ChronicDropdown";
 
 
 type isOptionSelectedType = (option: string, questionNameToCheck: string) => boolean;
@@ -10,16 +12,18 @@ const WhatIsTheNatureOfPainNewOptionChronic = ({selectedOption, parentQuestionPa
     const currentQuestionPath = parentQuestionPath+"-"+selectedOption;
     return (  
         <div>
-            
-            
+
+    
             <RadioInputOption text={selectedOption} value={selectedOption} questionName={parentQuestionPath}/>
             {isOptionSelected(selectedOption,parentQuestionPath) && 
                 <div className="ml-10">
-                    <h3 className="mt-3 mb-3"> How long have been experiencing low back pain? </h3>
-                    <HowLongFor selectedOption="Years" parentQuestionPath={currentQuestionPath} isOptionSelected={isOptionSelected}/>
+                    <h3 className="mt-3 mb-3"> How many years have been experiencing low back pain? </h3>
+                    <ChronicDropdown selectedOption={selectedOption} parentQuestionPath={currentQuestionPath}/>
+                    {/* <HowLongFor selectedOption="Years" parentQuestionPath={currentQuestionPath} isOptionSelected={isOptionSelected}/> */}
                     
                 </div>
             }
+
         </div>
     );
 }
