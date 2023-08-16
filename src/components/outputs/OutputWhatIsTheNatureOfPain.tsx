a9ebae28c9d30cc95c62dce6967be89a13cd6ecb
Seth Matthews <98sethmatthews@gmail.com>
2023-08-16 11:02:46 +1000

    results working without null assertion

--- src/components/outputs/OutputWhatIsTheNatureOfPain.tsx
+++ src/components/outputs/OutputWhatIsTheNatureOfPain.tsx
@@ -43,13 +43,15 @@ const OutputWhatIsTheNatureOfPain = ({isOptionSelected, resultsToDisplay }:formP
         numberOf = resultsToDisplay["What is the nature of pain?"+"-"+lowBackPainType+"-"+periodType]?.toString();
     }
     if (lowBackPainType===undefined || periodType===undefined || numberOf===undefined ){
-        throw new Error("lowBackPainType or periodType or numberOf is undefined ");
+        throw new Error("lowBackPainType or periodType or numberOf is undefined")
     }
 
     return (  
         <div>
             { 
             resultsToDisplay.hasOwnProperty("What is the nature of pain?") && 
+            resultsToDisplay.hasOwnProperty("What is the nature of pain?"+"-"+lowBackPainType) && 
+            resultsToDisplay.hasOwnProperty("What is the nature of pain?"+"-"+lowBackPainType+"-"+periodType) && 
             <p>
                 The patient has been experiencing {lowBackPainType} low back pain for the last {numberOf} {periodType}.
             </p>
